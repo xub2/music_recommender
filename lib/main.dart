894dfb63865ab3421eb0a8a151108116c0e7cf82
xub2 <victory628@gmail.com>
2024-05-29 18:16:56 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-
-import 'views/music_recommendation_screen/music_recommendation_screen.dart';
-import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
-
+import 'views/music_recommendation_screen/music_recommendation_screen.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyApp());
@@ -31,19 +29,19 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff19093C),
+      backgroundColor: Color(0xff19093C),  // 배경 색을 0xff19093C로 설정
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,  // 버튼을 중앙에 배치
           children: [
-            Spacer(),
+            Spacer(),  // 남은 공간을 채우기 위한 Spacer 위젯
             Container(
-              alignment: Alignment.center,
+              alignment: Alignment.center, // 텍스트를 중앙 정렬
               child: Text(
                 'Music\nRecommender',
-                textAlign: TextAlign.center,
-                style: GoogleFonts.lobster(
+                textAlign: TextAlign.center,  // 텍스트 중앙 정렬
+                style: GoogleFonts.lobster(  // Lobster 폰트 사용
                   textStyle: TextStyle(
                     fontSize: 65,
                     color: Colors.white,
@@ -51,48 +49,64 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(height: 50),
+            SizedBox(height: 50),  // 텍스트와 버튼 사이의 간격
             ElevatedButton(
               onPressed: () {
                 launchUrl(Uri.parse('https://open.spotify.com/'));
               },
               style: ElevatedButton.styleFrom(
                 padding: EdgeInsets.symmetric(horizontal: 100, vertical: 16),
-                textStyle: TextStyle(fontSize: 20),
+                textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
                 foregroundColor: Colors.green,
               ),
-              child: Text('Spotify'),
-            ),SizedBox(height: 20),
+              child: Text('🎵 Spotify'),
+            ),
+            SizedBox(height: 20),  // 버튼 사이에 간격 추가
             ElevatedButton(
               onPressed: () {
-
+                // 버튼을 눌렀을 때 MusicRecommendationScreen으로 이동
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => MusicRecommendationScreen()),
                 );
               },
               style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 30, vertical: 16),
-                textStyle: TextStyle(fontSize: 20),
+                padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),  // 버튼 크기 조정
+                textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),  // 텍스트 스타일
+                foregroundColor: Color(0xff19093C),  // 폰트 색상 설정
               ),
-              child: Text('음악 추천을 받아볼까요?'),
+              child: Text('🔍 음악 추천을 받아볼까요?'),
             ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                // 버튼을 눌렀을 때 앱을 종료
-                SystemNavigator.pop();
-              },
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 100, vertical: 16),
-                textStyle: TextStyle(fontSize: 20),
-                foregroundColor: Colors.red,
-              ),
-              child: Text('나가기'),
+            SizedBox(height: 20),  // 버튼 사이에 간격 추가
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    SystemNavigator.pop();
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 16),  // 버튼 크기 반으로 조정
+                    textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),  // 텍스트 스타일
+                    foregroundColor: Colors.red,  // 폰트 색상 설정
+                  ),
+                  child: Text('🚪 나가기'),
+                ),
+                SizedBox(width: 10),  // 버튼 사이의 간격 추가
+                ElevatedButton(
+                  onPressed: () {
+                    launchUrl(Uri.parse('https://xuv2.notion.site/Music-Recommendation-by-Spotify-336c0ed599eb458496e06c4275a68d2b'));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 18),  // 버튼 크기 반으로 조정
+                    textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),  // 텍스트 스타일
+                    foregroundColor: Colors.blue,  // 폰트 색상 설정
+                  ),
+                  child: Text('🌐 Notion'),
+                ),
+              ],
             ),
-            SizedBox(height: 20),
-
-            SizedBox(height: 220),
+            SizedBox(height: 220),  // 버튼을 중앙보다 아래로 보내기 위해 추가한 공간
           ],
         ),
       ),
