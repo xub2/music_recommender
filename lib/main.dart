86ffa5361d03230fb0632292bbb77221ced8cd0b
xub2 <victory628@gmail.com>
2024-06-10 12:39:35 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -29,19 +29,19 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xff19093C),  // 배경 색을 0xff19093C로 설정
+      backgroundColor: Color(0xff19093C),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,  // 버튼을 중앙에 배치
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Spacer(),  // 남은 공간을 채우기 위한 Spacer 위젯
+            Spacer(),
             Container(
-              alignment: Alignment.center, // 텍스트를 중앙 정렬
+              alignment: Alignment.center,
               child: Text(
                 'Music\nRecommender',
-                textAlign: TextAlign.center,  // 텍스트 중앙 정렬
-                style: GoogleFonts.lobster(  // Lobster 폰트 사용
+                textAlign: TextAlign.center,
+                style: GoogleFonts.lobster(
                   textStyle: TextStyle(
                     fontSize: 65,
                     color: Colors.white,
@@ -49,39 +49,59 @@ class HomePage extends StatelessWidget {
                 ),
               ),
             ),
-            SizedBox(height: 50),  // 텍스트와 버튼 사이의 간격
-            ElevatedButton(
-              onPressed: () {
-                launchUrl(Uri.parse('https://open.spotify.com/'));
-              },
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 16),
-                textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                foregroundColor: Color(0xff19093C),
-                backgroundColor: Colors.white,
-                minimumSize: Size(300, 60),  // 버튼 최소 크기 설정
-              ),
-              child: Text('Spotify'),
+            SizedBox(height: 50),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    launchUrl(Uri.parse('https://open.spotify.com/'));
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                    padding: EdgeInsets.all(0),
+                    fixedSize: Size(150, 150),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(18.0),
+                    child: Image.asset(
+                      'assets/images/Spotify.png',
+                      fit: BoxFit.cover,
+                      width: 150,
+                      height: 150,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => MusicRecommendationScreen()),
+                    );
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                    padding: EdgeInsets.all(0),
+                    fixedSize: Size(150, 150),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(18.0),
+                    child: Image.asset(
+                      'assets/images/Search.png',
+                      fit: BoxFit.cover,
+                      width: 150,
+                      height: 150,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 20),  // 버튼 사이에 간격 추가
-            ElevatedButton(
-              onPressed: () {
-                // 버튼을 눌렀을 때 MusicRecommendationScreen으로 이동
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MusicRecommendationScreen()),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 16),  // 버튼 크기 조정
-                textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),  // 텍스트 스타일
-                foregroundColor: Color(0xff19093C),  // 폰트 색상 설정
-                backgroundColor: Colors.white,
-                minimumSize: Size(300, 60),  // 버튼 최소 크기 설정
-              ),
-              child: Text('음악 추천 받기'),
-            ),
-            SizedBox(height: 20),  // 버튼 사이에 간격 추가
+            SizedBox(height: 20),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -90,32 +110,48 @@ class HomePage extends StatelessWidget {
                     launchUrl(Uri.parse('https://xuv2.notion.site/Music-Recommendation-by-Spotify-336c0ed599eb458496e06c4275a68d2b'));
                   },
                   style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 16),  // 버튼 크기 조정
-                    textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),  // 텍스트 스타일
-                    foregroundColor: Color(0xff19093C),  // 폰트 색상 설정
-                    backgroundColor: Colors.white,
-                    minimumSize: Size(145, 60),  // 버튼 최소 크기 설정
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                    padding: EdgeInsets.all(0),
+                    fixedSize: Size(150, 150),
                   ),
-                  child: Text('Notion'),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(18.0),
+                    child: Image.asset(
+                      'assets/images/notion.png',
+                      fit: BoxFit.cover,
+                      width: 150,
+                      height: 150,
+                    ),
+                  ),
                 ),
-                SizedBox(width: 10),
+                SizedBox(width: 20),
                 ElevatedButton(
                   onPressed: () {
                     SystemNavigator.pop();
                   },
                   style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 50, vertical: 16),  // 버튼 크기 조정
-                    textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),  // 텍스트 스타일
-                    foregroundColor: Colors.red,  // 폰트 색상 설정
-                    backgroundColor: Colors.white,
-                    minimumSize: Size(145, 60),  // 버튼 최소 크기 설정
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(18.0),
+                    ),
+                    padding: EdgeInsets.all(0),
+                    fixedSize: Size(150, 150),
                   ),
-                  child: Text('나가기'),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(18.0),
+                    child: Image.asset(
+                      'assets/images/Exit.png',
+                      fit: BoxFit.cover,
+                      width: 150,
+                      height: 150,
+                    ),
+                  ),
                 ),
-                // 버튼 사이의 간격 추가
               ],
             ),
-            SizedBox(height: 220),  // 버튼을 중앙보다 아래로 보내기 위해 추가한 공간
+            SizedBox(height: 20),
+            Spacer(),
           ],
         ),
       ),
