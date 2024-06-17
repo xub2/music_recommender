4bd30ee94a5037afcfa2743811a081ff0b5d6991
xub2 <victory628@gmail.com>
2024-06-17 12:04:47 +0900

    update

---
+++ lib/constant/strings.dart
@@ -0,0 +1,4 @@
+class CustomStrings {
+  static const String clientId = '1343542818a348f290f24a729a8e19ff';
+  static const String clientSecret = '33e80efc30604e088e8eb82ace880fbc';
+}
---
+++ lib/view_2/music_player.dart
@@ -0,0 +1,255 @@
+import 'package:audio_video_progress_bar/audio_video_progress_bar.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:music_recommender/constant/colors.dart';
+import 'package:music_recommender/constant/strings.dart';
+import 'package:music_recommender/model/music.dart';
+import 'package:music_recommender/view_2/lyric_page.dart';
+import 'package:palette_generator/palette_generator.dart';
+import 'package:spotify/spotify.dart';
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
+import 'widgets/art_work_image.dart';
+
+class MusicPlayer extends StatefulWidget {
+  const MusicPlayer({super.key});
+
+  @override
+  State<MusicPlayer> createState() => _MusicPlayerState();
+}
+
+class _MusicPlayerState extends State<MusicPlayer> {
+  final player = AudioPlayer();
+  Music music = Music(trackId: '2DwUdMJ5uxv20EhAildreg');
+
+  @override
+  void dispose() {
+    player.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    final credentials = SpotifyApiCredentials(
+        CustomStrings.clientId, CustomStrings.clientSecret);
+    final spotify = SpotifyApi(credentials);
+    spotify.tracks.get(music.trackId).then((track) async {
+      String? tempSongName = track.name;
+      if (tempSongName != null) {
+        music.songName = tempSongName;
+        music.artistName = track.artists?.first.name ?? "";
+        String? image = track.album?.images?.first.url;
+        if (image != null) {
+          music.songImage = image;
+          final tempSongColor = await getImagePalette(NetworkImage(image));
+          if (tempSongColor != null) {
+            music.songColor = tempSongColor;
+          }
+        }
+        music.artistImage = track.artists?.first.images?.first.url;
+        final yt = YoutubeExplode();
+        final video = (await yt.search.search("$tempSongName ${music.artistName ?? ""}")).first;
+        final videoId = video.id.value;
+        music.duration = video.duration;
+        setState(() {});
+        var manifest = await yt.videos.streamsClient.getManifest(videoId);
+        var audioUrl = manifest.audioOnly.last.url;
+        player.play(UrlSource(audioUrl.toString()));
+      }
+    });
+    super.initState();
+  }
+
+  Future<Color?> getImagePalette(ImageProvider imageProvider) async {
+    final PaletteGenerator paletteGenerator =
+    await PaletteGenerator.fromImageProvider(imageProvider);
+    return paletteGenerator.dominantColor?.color;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      backgroundColor: const Color(0xff19093C), // 배경 색상 설정
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 26),
+          child: Column(
+            children: [
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Icon(Icons.close, color: Colors.transparent),
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Text(
+                        '지금 재생중',
+                        style: textTheme.bodyMedium?.copyWith(
+                          color: Colors.white,
+                          fontSize: 20, // 폰트 크기 증가
+                        ),
+                      ),
+                      const SizedBox(height: 6),
+                      Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.white,
+                            backgroundImage: music.artistImage != null
+                                ? NetworkImage(music.artistImage!)
+                                : null,
+                            radius: 12, // 크기 약간 증가
+                          ),
+                          const SizedBox(width: 4),
+                          Text(
+                            music.artistName ?? '-',
+                            style: textTheme.bodyLarge?.copyWith(
+                              color: Colors.white,
+                              fontSize: 18, // 폰트 크기 증가
+                            ),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                  const Icon(
+                    Icons.close,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+              Expanded(
+                flex: 2,
+                child: Center(
+                  child: ArtWorkImage(image: music.songImage),
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              music.songName ?? '',
+                              style: textTheme.titleLarge?.copyWith(
+                                color: Colors.white,
+                                fontSize: 24, // 폰트 크기 증가
+                              ),
+                            ),
+                            Text(
+                              music.artistName ?? '-',
+                              style: textTheme.titleMedium?.copyWith(
+                                color: Colors.white60,
+                                fontSize: 20, // 폰트 크기 증가
+                              ),
+                            ),
+                          ],
+                        ),
+                        const Icon(
+                          Icons.favorite,
+                          color: CustomColors.primaryColor,
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 16),
+                    StreamBuilder(
+                      stream: player.onPositionChanged,
+                      builder: (context, data) {
+                        return ProgressBar(
+                          progress: data.data ?? const Duration(seconds: 0),
+                          total: music.duration ?? const Duration(minutes: 4),
+                          bufferedBarColor: Colors.white38,
+                          baseBarColor: Colors.white10,
+                          thumbColor: Colors.white,
+                          timeLabelTextStyle: const TextStyle(
+                            color: Colors.white,
+                            fontSize: 18,
+                          ),
+                          progressBarColor: Colors.white,
+                          onSeek: (duration) {
+                            player.seek(duration);
+                          },
+                        );
+                      },
+                    ),
+                    const SizedBox(height: 16),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => LyricsPage(
+                                  music: music,
+                                  player: player,
+                                ),
+                              ),
+                            );
+                          },
+                          icon: const Icon(
+                            Icons.lyrics_outlined,
+                            color: Colors.white,
+                            size: 30, // 아이콘 크기 증가
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.skip_previous,
+                            color: Colors.white,
+                            size: 40, // 아이콘 크기 증가
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () async {
+                            if (player.state == PlayerState.playing) {
+                              await player.pause();
+                            } else {
+                              await player.resume();
+                            }
+                            setState(() {});
+                          },
+                          icon: Icon(
+                            player.state == PlayerState.playing
+                                ? Icons.pause
+                                : Icons.play_circle,
+                            color: Colors.white,
+                            size: 70, // 아이콘 크기 증가
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.skip_next,
+                            color: Colors.white,
+                            size: 40, // 아이콘 크기 증가
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {},
+                          icon: const Icon(
+                            Icons.loop,
+                            color: CustomColors.primaryColor,
+                            size: 30, // 아이콘 크기 증가
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
